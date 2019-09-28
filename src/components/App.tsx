c5a0f5d6a5b628e44098e8b81c086a2117968e0b
kuroda-kentaro <kuroda_business@outlook.jp>
2019-09-28 20:42:29 +0900

    introduce_emotion

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,34 @@
+/** @jsx jsx */
 import React from 'react';
-import './App.css';
+import { css, jsx } from '@emotion/core';
+
+const app = css`
+  text-align: center;
+`;
+
+const appHeader = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const appLink = css`
+  color: #09d3ac;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div css={app}>
+      <header css={appHeader}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a css={appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
