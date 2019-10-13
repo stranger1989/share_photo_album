8db58bd63320c5159eafa7b94bfe89eae13a1fc8
kuroda-kentaro <kuroda_business@outlook.jp>
2019-10-13 18:57:01 +0900

    create_image_upload_form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import TextField from '@material-ui/core/TextField';
+import DialogActions from '@material-ui/core/DialogActions';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+import Previews from './ImageUpload';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    field: {
+      marginTop: 20,
+    },
+  }),
+);
 
 const sleep = (ms: number) => new Promise((resolve: any) => setTimeout(resolve, ms));
 
@@ -36,9 +49,11 @@ const renderTextField: React.FC<any> = ({ label, input, meta: { touched, invalid
   <TextField
     fullWidth
     label={label}
-    placeholder={label}
     error={touched && invalid}
     helperText={touched && error}
+    InputLabelProps={{
+      shrink: true,
+    }}
     {...input}
     {...custom}
   />
@@ -46,7 +61,7 @@ const renderTextField: React.FC<any> = ({ label, input, meta: { touched, invalid
 
 const renderSelectField: React.FC<any> = ({ input, label, meta: { touched, error }, children, ...custom }) => (
   <FormControl error={touched && error}>
-    <InputLabel htmlFor="visible">Visible</InputLabel>
+    <InputLabel htmlFor="visible" shrink={true}>Visible</InputLabel>
     <Select
       native
       {...input}
@@ -63,37 +78,50 @@ const renderSelectField: React.FC<any> = ({ input, label, meta: { touched, error
 );
 
 const MaterialUiForm = (props: any) => {
-  const { handleSubmit, pristine, reset, submitting, classes } = props;
+  const { handleSubmit, pristine, reset, submitting } = props;
+  const [resetStatue, resetTriger] = React.useState(false);
+  const classes = useStyles();
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <Field name="title" component={renderTextField} label="Title" />
       </div>
-      <div>
-        <Field classes={classes} name="visible" component={renderSelectField} label="Visible">
-          <option value="public">public</option>
+      <div className={classes.field}>
+        <Field name="visible" component={renderSelectField} label="Visible">
+          <option value="public" defaultChecked>public</option>
           <option value="private">private</option>
         </Field>
       </div>
-      <div />
+      <div>
+        <Field
+          name="imageToUpload"
+          component={Previews}
+          type="file"
+          value={null}
+          reset={resetStatue}
+          resetTriger={resetTriger}
+        />
+      </div>
       <div>
         <Field name="notes" component={renderTextField} label="Notes" multiline rowsMax="4" margin="normal" />
       </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
+      <DialogActions>
+        <Button type="submit" disabled={pristine || submitting} color="primary">
           Submit
-        </button>
-        <button
+        </Button>
+        <Button
           type="button"
           disabled={pristine || submitting}
+          color="secondary"
           onClick={() => {
             reset();
+            resetTriger(true);
           }}
         >
           Clear Values
-        </button>
-      </div>
+        </Button>
+      </DialogActions>
     </form>
   );
 };
