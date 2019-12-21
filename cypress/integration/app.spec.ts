691d47935d48d47f7dbd3ba43a9b687565806acd
kuroda-kentaro <kuroda_business@outlook.jp>
2019-12-21 18:19:32 +0900

    revise_cypress_testcode

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -3,10 +3,14 @@ describe('page transition', () => {
 
   beforeEach(() => {
     cy.visit(baseUrl);
+    cy.get('input[data-test="username-input"]').type('test');
+    cy.get('input[data-test="sign-in-password-input"]').type('test1234');
+    cy.get('button[data-test="sign-in-sign-in-button"]').click();
   });
 
-  it('page transition', () => {
-    // cy.get('').click();
-    // cy.location('pathname').should('eq', '');
+  it('ログインのテスト', () => {});
+
+  it('ログアウトのテスト', () => {
+    cy.get('button[data-test="sign-out-button"]').click();
   });
 });
